c38c83e9984f955a8c00458aaebd329c3dbe1655
ANDREW-ALUNO <andrew.silva@alu.apac.org.br>
2023-08-09 18:14:35 -0300

    Feed Feito :)

--- navigation/tabNavigator.js
+++ navigation/tabNavigator.js
@@ -1,36 +1,62 @@
 import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Feed from '../screens/feed'
 import CreateStory from '../screens/createStory'
 import Ionicons from 'react-native-vector-icons/Ionicons'
-const Tab = createBottomTabNavigator()
+import { RFValue } from "react-native-responsive-fontsize";
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { StyleSheet } from 'react-native';
+
+const Tab = createMaterialBottomTabNavigator();
+
 
 export default function TabNavigator() {
-    return (
-      
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-  
-              if (route.name === 'Feed') {
-                iconName = focused
-                  ?'book'
-                  : 'book-outline';
-              } else if (route.name === 'CreateStory') {
-                iconName = focused ? 'create' : 'create-outline';
-              }
-  
-              // You can return any component that you like here!
-              return <Ionicons name={iconName} size={size} color={color} />;
-            },
-            tabBarActiveTintColor: 'tomato',
-            tabBarInactiveTintColor: 'gray',
-          })}
-        >
-          <Tab.Screen name="Feed" component={Feed} />
-          <Tab.Screen name="CreateStory" component={CreateStory} />
-        </Tab.Navigator>
-      
-    );
+  return (
+
+    <Tab.Navigator
+      labeled={false}
+      barStyle={styles.bottomTabStyle}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Feed') {
+            iconName = focused
+              ? 'book'
+              : 'book-outline';
+          } else if (route.name === 'CreateStory') {
+            iconName = focused ? 'create' : 'create-outline';
+          }
+
+          // You can return any component that you like here!
+          return (<Ionicons
+            name={iconName}
+            size={RFValue(25)}
+            color={color}
+            style={styles.icons}
+          />)
+
+        },
+      })}
+      activeColor={"#ee8249"}
+      inactiveColor={"gray"}
+    >
+      <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="CreateStory" component={CreateStory} />
+    </Tab.Navigator>
+
+  );
+}
+const styles = StyleSheet.create({
+  bottomTabStyle: {
+    backgroundColor: "#2f345d",
+    height: "8%",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    overflow: "hidden",
+    position: "absolute"
+  },
+  icons: {
+    width: RFValue(30),
+    height: RFValue(30)
   }
+});
