a6ac960e532d73ee854db181d29aee3bf86c66a4
ANDREW-ALUNO <andrew.silva@alu.apac.org.br>
2023-08-30 18:12:39 -0300

    YAY :D

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import firebase from 'firebase';
+import { firebaseConfig } from './config';
 import { NavigationContainer } from '@react-navigation/native';
 import DrawerNavigator from './navigation/drawerNavigator';
+import { createStackNavigator } from '@react-navigation/stack';
 
+if (! firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+} else {
+  firebase.app()
+}
+
+const Stack = createStackNavigator()
+const MyStack=()=>{
+  return (
+    <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown:false,gestureEnabled:false}}>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="DashBoard" component={DrawerNavigator} />
+
+    </Stack.Navigator>
+  );
+}
 export default function App() {
   return (
     <NavigationContainer>
-      <DrawerNavigator/>
+      <MyStack/>
     </NavigationContainer>
   );
 }
---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDEupoWtz_RYL5SnzxATvzxVkFWD8SzMsc",
+    authDomain: "storytelling-3f65b.firebaseapp.com",
+    databaseURL: "https://storytelling-3f65b-default-rtdb.firebaseio.com",
+    projectId: "storytelling-3f65b",
+    storageBucket: "storytelling-3f65b.appspot.com",
+    messagingSenderId: "1066317294",
+    appId: "1:1066317294:web:5e4ae03b27f6a01566caff"
+  };
